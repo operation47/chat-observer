c1b2250921c7d6b463e80b23d6611894eed36132
Mikka <mikka@sendke.eu>
2023-10-07 15:11:56 +0200

    Switched to es modules and added pg

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-const tmi = require('tmi.js');
+import * as tmi from 'tmi.js';
+import * as pg from 'pg';
 
 // TODO: add db connection
 
 const botName = process.env.BOT_NAME;
 const botOauth = process.env.BOT_OAUTH;
-const lookFor = process.env.LOOK_FOR_USER.split(',');
-const lookIn = process.env.LOOK_IN_CHANNEL.split(',');
+
+const lookFor = ['username1', 'username2'];
+const lookIn = ['channel1', 'channel2'];
+
+if (!process.env.BOT_NAME || !process.env.BOT_OAUTH) {
+  console.error('One or more required environment variables are missing.');
+  process.exit(1);
+}
+
+const dbClient = new pg.Client();
+await dbClient.connect();
+console.log('Connected to DB');
 
 const client = new tmi.Client({
 	identity: {
@@ -14,18 +25,24 @@ const client = new tmi.Client({
 	}
 });
 
-client.connect();
+await client.connect();
+console.log('Connected to Twitch');
+
+for (const channel of lookIn) {
+    console.log(`Joining ${channel}`);
+    client.join(channel);
+}
 
 client.on('message', (channel, tags, message, self) => {
     if (self) return;
-    if (lookFor.includes(tags.username)) {
-        const msg = { 
-            username: tags.username,
-            channel: channel,
-            message: message,
-            // timestamp: (don't know if a timestamp exists in tags)
-        };
-        // TODO: save msg to db
-    }
+    if (!lookFor.includes(tags.username)) return;
+    const entry = { 
+        username: tags.username,
+        channel: channel,
+        displayName: tags['display-name'],
+        message: message,
+        timestamp: tags['tmi-sent-ts']
+    };
+    console.log(entry);
 });
 
