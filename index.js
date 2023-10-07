7c3613d953ce25e20c82bb2eed2abd5a4876b206
Mikka <mikka@sendke.eu>
2023-10-07 04:51:48 +0200

    besser maybe

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const tmi = require('tmi.js');
 
-const port = process.env.PORT || 3000;
+// TODO: add db connection
+
 const botName = process.env.BOT_NAME;
 const botOauth = process.env.BOT_OAUTH;
+const lookFor = process.env.LOOK_FOR_USER.split(',');
+const lookIn = process.env.LOOK_IN_CHANNEL.split(',');
 
 const client = new tmi.Client({
 	identity: {
@@ -14,5 +17,15 @@ const client = new tmi.Client({
 client.connect();
 
 client.on('message', (channel, tags, message, self) => {
+    if (self) return;
+    if (lookFor.includes(tags.username)) {
+        const msg = { 
+            username: tags.username,
+            channel: channel,
+            message: message,
+            // timestamp: (don't know if a timestamp exists in tags)
+        };
+        // TODO: save msg to db
+    }
 });
 
