866a75eb777220a96d8d5ae80f4d2b2a52e819fd
Mikka <mikka@sendke.eu>
2023-10-07 16:20:59 +0200

    Fixed and added things

--- index.js
+++ index.js
@@ -1,35 +1,39 @@
 import * as tmi from 'tmi.js';
-import * as pg from 'pg';
+import pg from 'pg';
 
-// TODO: add db connection
-
+const isProduction = process.env.NODE_ENV === 'production';
 const botName = process.env.BOT_NAME;
 const botOauth = process.env.BOT_OAUTH;
 
-const lookFor = ['username1', 'username2'];
-const lookIn = ['channel1', 'channel2'];
+let client;
+const lookFor = ['swox___', 'username2'];
+const lookIn = ['stegi', 'channel2'];
 
 if (!process.env.BOT_NAME || !process.env.BOT_OAUTH) {
-  console.error('One or more required environment variables are missing.');
-  process.exit(1);
+    console.error('No bot name or oauth token provided, logging in anonymously.');
+    client = new tmi.Client();
+}
+else {
+    client = new tmi.Client({
+        identity: {
+            username: botName,
+            password: botOauth,
+        },
+    });
 }
 
-const dbClient = new pg.Client();
-await dbClient.connect();
+let dbClient;
+if (isProduction) {
+    dbClient = new pg.Client();
+    await dbClient.connect();
+}
 console.log('Connected to DB');
 
-const client = new tmi.Client({
-	identity: {
-		username: botName,
-		password: botOauth
-	}
-});
-
 await client.connect();
-console.log('Connected to Twitch');
+console.log('Connected to Twitch.');
 
 for (const channel of lookIn) {
-    console.log(`Joining ${channel}`);
+    console.log(`Joining ${channel}.`);
     client.join(channel);
 }
 
@@ -37,12 +41,16 @@ client.on('message', (channel, tags, message, self) => {
     if (self) return;
     if (!lookFor.includes(tags.username)) return;
     const entry = { 
-        username: tags.username,
+        timestamp: tags['tmi-sent-ts'],
         channel: channel,
+        user: tags.username,
+        content: message,
         displayName: tags['display-name'],
-        message: message,
-        timestamp: tags['tmi-sent-ts']
     };
-    console.log(entry);
+    if (isProduction) { 
+        // TODO: insert into db
+        return;
+    }
+    console.log(JSON.stringify(entry));
 });
 
